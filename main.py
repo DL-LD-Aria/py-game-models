86898ea6979d83bb6e914277e0b5ddaba50d50e5
Dasha <dariia.leshchenko@gmail.com>
2025-09-06 15:25:43 +0300

    Solution

--- main.py
+++ main.py
@@ -5,35 +5,39 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open('players.json', 'r', encoding='utf-8') as file:
+    with open("players.json", "r", encoding="utf-8") as file:
         players_data = json.load(file)
 
     for nickname, player_data in players_data.items():
         race, created = Race.objects.get_or_create(
-            name=player_data['race']['name'],
-            defaults={'description': player_data['race'].get('description','')},
+            name=player_data["race"]["name"],
+            defaults={
+                "description": player_data["race"].get("description", "")
+            },
         )
-        if player_data['race']['skills']:
-            for skill_data in player_data['race']['skills']:
+        if player_data["race"]["skills"]:
+            for skill_data in player_data["race"]["skills"]:
                 skill, created = Skill.objects.get_or_create(
-                    name=skill_data['name'],
+                    name=skill_data["name"],
                     race=race,
-                    defaults={'bonus': skill_data['bonus']}
+                    defaults={"bonus": skill_data["bonus"]}
                 )
 
         guild = None
-        if player_data.get('guild') and player_data['guild'] is not None:
+        if player_data.get("guild") and player_data["guild"] is not None:
             guild, created = Guild.objects.get_or_create(
-                name=player_data['guild']['name'],
-                defaults={'description': player_data['guild'].get('description')}
+                name=player_data["guild"]["name"],
+                defaults={
+                    "description": player_data["guild"].get("description")
+                }
             )
         player, created = Player.objects.get_or_create(
             nickname=nickname,
             defaults={
-                'email': player_data['email'],
-                'bio': player_data['bio'],
-                'race': race,
-                'guild': guild
+                "email": player_data["email"],
+                "bio": player_data["bio"],
+                "race": race,
+                "guild": guild
             }
         )
         if created:
